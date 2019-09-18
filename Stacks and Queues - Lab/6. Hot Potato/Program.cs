f984d67728140826f0907046ea37c18388e401ef
drapperr <drapperr@users.noreply.github.com>
2019-09-18 21:31:01 +0300

    update

---
+++ Stacks and Queues - Lab/6. Hot Potato/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Queue<string> kids = new Queue<string>(input);
+            int count = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < count - 1; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/7. Traffic Jam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            List<string> passedCars = new List<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    int currentCount = Math.Min(queue.Count, count);
+                    for (int i = 0; i < currentCount; i++)
+                    {
+                        passedCars.Add(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, passedCars.Select(x => $"{x} passed!")));
+            Console.WriteLine($"{passedCars.Count} cars passed the crossroads.");
+        }
+    }
+}
