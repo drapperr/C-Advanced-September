2a499c0783d4717f62b869a2feeabc94555f3ba8
drapperr <drapperr@users.noreply.github.com>
2019-09-19 00:17:44 +0300

    update

--- Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
+++ Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08._Balanced_Parenthesis
 {
@@ -6,7 +8,61 @@ namespace _08._Balanced_Parenthesis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            Stack<char> leftSide = new Stack<char>(input);
+            Stack<char> rightSide = new Stack<char>();
+
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                char ch = leftSide.Pop();
+                if (ch == '{')
+                {
+                    rightSide.Push('}');
+                }
+                else if (ch == '}')
+                {
+                    rightSide.Push('{');
+                }
+                else if (ch == '[')
+                {
+                    rightSide.Push(']');
+                }
+                else if (ch == ']')
+                {
+                    rightSide.Push('[');
+                }
+                else if (ch == '(')
+                {
+                    rightSide.Push(')');
+                }
+                else if (ch == ')')
+                {
+                    rightSide.Push('(');
+                }
+                else
+                {
+                    rightSide.Push(leftSide.Pop());
+                }
+            }
+            bool balanced = true;
+
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                if (leftSide.Pop()!=rightSide.Pop())
+                {
+                    balanced = false;
+                    break;
+                }
+            }
+
+            if (balanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
