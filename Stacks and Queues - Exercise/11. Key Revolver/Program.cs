32f217640c927695108e6bfb9b932f3078d981cc
drapperr <drapperr@users.noreply.github.com>
2019-09-21 20:45:27 +0300

    update

--- Stacks and Queues - Exercise/11. Key Revolver/Program.cs
+++ Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11._Key_Revolver
 {
@@ -6,7 +8,43 @@ namespace _11._Key_Revolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int sizeGunBarrel = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+            Queue<int> locks = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+            int value = int.Parse(Console.ReadLine());
+
+            int bulletsCounter = 0;
+            while (bullets.Count != 0 && locks.Count != 0)
+            {
+                if (bullets.Pop() <= locks.Peek())
+                {
+                    locks.Dequeue();
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                bulletsCounter++;
+                if (bulletsCounter % sizeGunBarrel == 0 && bullets.Count != 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+            if (locks.Count == 0)
+            {
+                int earnedMoney = value - bulletsCounter * bulletPrice;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${earnedMoney}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
         }
     }
 }
---
+++ Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> cups = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+            Stack<int> bottles = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+            int wastedWater = 0;
+
+            while (cups.Count>0)
+            {
+                int currentCup = cups.Peek();
+                int currentBottle = 0;
+
+                if (bottles.Count > 0)
+                {
+                    currentBottle = bottles.Pop();
+                }
+                else break;
+
+                int currentWastedWater = currentBottle - currentCup;
+
+                while (currentWastedWater<0)
+                {
+                    if (bottles.Count > 0)
+                    {
+                        currentWastedWater += bottles.Pop();
+                    }
+                    else break;
+                }
+                if (currentWastedWater >= 0)
+                {
+                    wastedWater += currentWastedWater;
+                }
+                else break;
+                cups.Dequeue();
+            }
+            if (cups.Count!=0)
+            {
+                Console.Write("Cups: ");
+                Console.WriteLine(string.Join(" ",cups));
+            }
+            else
+            {
+                Console.Write("Bottles: ");
+                Console.WriteLine(string.Join(" ",bottles));
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
