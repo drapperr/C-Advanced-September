8cc5e3979bb94e6dff3549f7614789363675be39
drapperr <drapperr@users.noreply.github.com>
2019-09-24 16:08:50 +0300

    update

---
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _4._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var matrix = new int[num, num];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string line = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row,col]==symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixParts = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixParts[0];
+            int cols = matrixParts[1];
+            var matrix = new int[rows,cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var numbers = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            int maxSum = int.MinValue;
+            int topLeftIndexRow = 0;
+            int topLeftIndexCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0)-1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (currentSum>maxSum)
+                    {
+                        maxSum = currentSum;
+                        topLeftIndexRow = row;
+                        topLeftIndexCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"{matrix[topLeftIndexRow,topLeftIndexCol]} {matrix[topLeftIndexRow, topLeftIndexCol+1]}");
+            Console.WriteLine($"{matrix[topLeftIndexRow+1, topLeftIndexCol]} {matrix[topLeftIndexRow+1, topLeftIndexCol+1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
