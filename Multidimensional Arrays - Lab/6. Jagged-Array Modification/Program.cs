ff0cf523b9a29ed0ef8461c65e98953bf2fb5649
drapperr <drapperr@users.noreply.github.com>
2019-09-24 16:45:33 +0300

    update

---
+++ Multidimensional Arrays - Lab/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            var jaggedArr = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+                jaggedArr[i] = line;
+            }
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] splitedInput = input.Split(' ');
+                string command = splitedInput[0];
+                int row = int.Parse(splitedInput[1]);
+                int col = int.Parse(splitedInput[2]);
+                int value = int.Parse(splitedInput[3]);
+
+                if (row < 0 || row >= jaggedArr.Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+                if (col < 0 || col >= jaggedArr[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (command== "Add")
+                {
+                    jaggedArr[row][col] += value;
+                }
+                else if (command== "Subtract")
+                {
+                    jaggedArr[row][col] -= value;
+                }
+            }
+            foreach (var arr in jaggedArr)
+            {
+                foreach (var number in arr)
+                {
+                    Console.Write(number+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _7._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var jaggedArr = new int[num][];
+            jaggedArr[0] = new int[]{ 1};
+
+            for (int i = 1; i < num; i++)
+            {
+                jaggedArr[i] = new int[i + 1];
+                jaggedArr[i][0] = 1;
+                jaggedArr[i][jaggedArr[i].Length - 1] = 1;
+
+                for (int j = 1; j < jaggedArr[i].Length-1; j++)
+                {
+                    jaggedArr[i][j] = jaggedArr[i - 1][j] + jaggedArr[i - 1][j - 1];
+                }
+            }
+            foreach (var arr in jaggedArr)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+            }
+        }
+    }
+}
