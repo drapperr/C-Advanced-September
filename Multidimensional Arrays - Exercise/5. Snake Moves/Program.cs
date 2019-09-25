ee18b5ad8656c8714b455c2f6822a2b21306906a
drapperr <drapperr@users.noreply.github.com>
2019-09-25 20:12:56 +0300

    update

---
+++ Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Snake_Moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixArgs = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixArgs[0];
+            int cols = matrixArgs[1];
+            char[,] matrix = new char[rows, cols];
+            Queue<char> snake = new Queue<char>(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row%2==0)
+                    {
+                        matrix[row, col] = snake.Peek();
+                    }
+                    else
+                    {
+                        matrix[row, matrix.GetLength(1) - 1 - col] = snake.Peek();
+                    }
+                    snake.Enqueue(snake.Dequeue());
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays - Exercise/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            double[][] jaggedArray = new double[rows][];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                double[] line = Console.ReadLine()
+                    .Split()
+                    .Select(double.Parse)
+                    .ToArray();
+                jaggedArray[i] = line;
+            }
+
+            for (int i = 0; i < jaggedArray.Length-1; i++)
+            {
+                if (jaggedArray[i].Length==jaggedArray[i+1].Length)
+                {
+                    for (int j = 0; j < jaggedArray[i].Length; j++)
+                    {
+                        jaggedArray[i][j] *= 2;
+                        jaggedArray[i + 1][j] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < jaggedArray[i].Length; j++)
+                    {
+                        jaggedArray[i][j] /= 2;
+                    }
+                    for (int j = 0; j < jaggedArray[i+1].Length; j++)
+                    {
+                        jaggedArray[i + 1][j] /= 2;
+                    }
+                }
+            }
+            string input = string.Empty;
+
+            while ((input=Console.ReadLine())!="End")
+            {
+                string[] inputArgs = input.Split();
+                string command = inputArgs[0];
+                int row = int.Parse(inputArgs[1]);
+                int col = int.Parse(inputArgs[2]);
+                int value = int.Parse(inputArgs[3]);
+
+                if (row >= 0 && row < jaggedArray.Length)
+                {
+                    if (col >= 0 && col < jaggedArray[row].Length)
+                    {
+                        if (command=="Add")
+                        {
+                            jaggedArray[row][col] += value;
+                        }
+                        else if (command=="Subtract")
+                        {
+                            jaggedArray[row][col] -= value;
+                        }
+                    }
+                    else continue;
+                }
+                else continue;
+            }
+
+            foreach (var arr in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+            }
+        }
+    }
+}
