f7a3aba1079961883569597fec51c42bfaa74e23
drapperr <drapperr@users.noreply.github.com>
2019-09-29 22:03:27 +0300

    update

---
+++ Sets and Dictionaries Advanced - Lab/05. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uniNames = new HashSet<string>();
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                uniNames.Add(Console.ReadLine());
+            }
+            foreach (var name in uniNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
