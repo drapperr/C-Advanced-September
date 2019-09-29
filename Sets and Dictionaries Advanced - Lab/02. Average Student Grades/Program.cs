fac733ccb9710b4901e5dda0fbd9268f19e12eb5
drapperr <drapperr@users.noreply.github.com>
2019-09-29 22:02:18 +0300

    update

---
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var grades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var studentArgs = Console.ReadLine().Split();
+                var name = studentArgs[0];
+                var grade = double.Parse(studentArgs[1]);
+
+                if (!grades.ContainsKey(name))
+                {
+                    grades[name] = new List<double>();
+                }
+                grades[name].Add(grade);
+            }
+            foreach (var student in grades)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(' ',student.Value.Select(x=>$"{x:F2}"))} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
