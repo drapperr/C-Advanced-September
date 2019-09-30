c85e40e80fed1f2acd17840796fcb49d269e1df1
drapperr <drapperr@users.noreply.github.com>
2019-09-30 19:50:22 +0300

    update

---
+++ Sets and Dictionaries Advanced - Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reservNumbers = new HashSet<string>();
+            var vipNumbers = new HashSet<string>();
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    vipNumbers.Add(input);
+                }
+                else
+                {
+                    reservNumbers.Add(input);
+                }
+            }
+            input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                reservNumbers.Remove(input);
+                vipNumbers.Remove(input);
+            }
+            Console.WriteLine(vipNumbers.Count+reservNumbers.Count);
+            foreach (var number in vipNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            foreach (var number in reservNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
