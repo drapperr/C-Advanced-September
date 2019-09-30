cda5b03406c8e906f19babad57fa39f71f57dc3b
drapperr <drapperr@users.noreply.github.com>
2019-09-30 19:51:16 +0300

    update

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                foreach (var element in line)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(" ",periodicTable));
+        }
+    }
+}
