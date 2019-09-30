3896e573a1f95937cf8e88fb655df065b5d0e5e1
drapperr <drapperr@users.noreply.github.com>
2019-09-30 19:50:46 +0300

    update

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var userNames = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                userNames.Add(Console.ReadLine());
+            }
+            foreach (var name in userNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
