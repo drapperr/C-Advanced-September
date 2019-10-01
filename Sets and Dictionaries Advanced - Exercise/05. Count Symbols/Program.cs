b78427481daf09edbebeee6bf639b0aa957e12fd
drapperr <drapperr@users.noreply.github.com>
2019-10-01 17:58:01 +0300

    update

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charsCount = new SortedDictionary<char, int>();
+            var text = Console.ReadLine();
+
+            foreach (var ch in text)
+            {
+                if (!charsCount.ContainsKey(ch))
+                {
+                    charsCount.Add(ch, 0);
+                }
+                charsCount[ch]++;
+            }
+            foreach (var kvp in charsCount)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
