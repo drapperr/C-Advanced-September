b99cd2a07a296893db9c5053101663395fb85bc9
drapperr <drapperr@users.noreply.github.com>
2019-10-02 20:28:22 +0300

    update

---
+++ STREAMS, FILES AND DIRECTORIES - Lab/2. Line Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    int counter = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
