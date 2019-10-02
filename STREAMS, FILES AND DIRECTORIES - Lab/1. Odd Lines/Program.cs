d23fc1c46999341d34843630b73cfe323f144bbb
drapperr <drapperr@users.noreply.github.com>
2019-10-02 20:27:50 +0300

    update

---
+++ STREAMS, FILES AND DIRECTORIES - Lab/1. Odd Lines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _1._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    int counter = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (counter % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
+
