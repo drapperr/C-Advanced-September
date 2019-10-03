1dcaa25f644d23f287c4d29ad187e85198e790cd
drapperr <drapperr@users.noreply.github.com>
2019-10-03 21:36:32 +0300

    update

---
+++ Streams, Files and Directories - Exercise/Problem 1. Even Lines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Problem_1._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols = new char[] { '-', ',', '.', '!', '?' };
+            int lineNum = 0;
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine().Split(' ');
+                    if (lineNum % 2 == 0)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(string.Join(" ", line.Reverse()));
+                        for (int i = 0; i < sb.Length; i++)
+                        {
+                            if (symbols.Contains(sb[i]))
+                            {
+                                sb[i] = '@';
+                            }
+                        }
+                        Console.WriteLine(sb);
+                    }
+                    lineNum++;
+                }
+            }
+        }
+    }
+}
