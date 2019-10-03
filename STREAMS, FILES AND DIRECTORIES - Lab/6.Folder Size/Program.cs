613a14df4185a2add3c0474fb10cf20924d756f6
drapperr <drapperr@users.noreply.github.com>
2019-10-03 21:35:30 +0300

    update

---
+++ STREAMS, FILES AND DIRECTORIES - Lab/6.Folder Size/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _6.Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"..\..\..\TestFolder");
+            var files =directoryInfo.GetFiles();
+            long sum = 0;
+            foreach (var file in files)
+            {
+                sum+=file.Length;
+            }
+            Console.WriteLine(sum/1024.0/1024.0);
+        }
+    }
+}
