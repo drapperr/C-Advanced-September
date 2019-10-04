c10dd33d85578ea3d8db7937dd5ce514fdfb8dcc
drapperr <drapperr@users.noreply.github.com>
2019-10-04 22:35:37 +0300

    update

---
+++ Streams, Files and Directories - Exercise/Problem 4. Copy Binary File/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Problem_4._Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream streamReader = new FileStream("copyMe.png", FileMode.Open))
+            {
+                using (FileStream streamWriter = new FileStream("copyMe-copy.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] byteArr = new byte[4096];
+
+                        int size = streamReader.Read(byteArr, 0, byteArr.Length);
+
+                        if (size==0)
+                        {
+                            break;
+                        }
+
+                        streamWriter.Write(byteArr, 0, size);
+                    }
+                }
+            }
+        }
+    }
+}
