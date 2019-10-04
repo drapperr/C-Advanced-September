5999c44786cb79218672a53350423a995c91c133
drapperr <drapperr@users.noreply.github.com>
2019-10-04 22:36:25 +0300

    update

---
+++ Streams, Files and Directories - Exercise/Problem 6. Zip and Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Problem_6._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(Environment.CurrentDirectory, @"..\..\..\zipFile.zip");
+            ZipFile.ExtractToDirectory(@"..\..\..\zipFile.zip", @"..\..\..\unzipped");
+        }
+    }
+}
