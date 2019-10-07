c144af812812ba6bb0f745cda780657e5f937c85
drapperr <drapperr@users.noreply.github.com>
2019-10-07 20:33:47 +0300

    update

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            var sortedWords = words.Where(x => char.IsUpper(x[0]));
+            Console.WriteLine(string.Join(Environment.NewLine,sortedWords));
+        }
+    }
+}
