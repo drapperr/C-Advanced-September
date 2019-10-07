6185ba40cf9ed0c08c90e4e219bc19fe197778b2
drapperr <drapperr@users.noreply.github.com>
2019-10-07 20:34:02 +0300

    update

---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x=>x*1.2)
+                .ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine,prices.Select(x=>$"{x:f2}")));
+        }
+    }
+}
