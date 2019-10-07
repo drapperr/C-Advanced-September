7bf0eb656ece773e6792c436849445c268321d74
drapperr <drapperr@users.noreply.github.com>
2019-10-07 20:33:15 +0300

    update

---
+++ Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x);
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
