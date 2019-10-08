40e439534a342183af723bb9c7a7432ecb229dd3
drapperr <drapperr@users.noreply.github.com>
2019-10-08 15:42:10 +0300

    update

---
+++ Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printToConsole = x=> 
+            {
+                foreach (var name in x.Split(' '))
+                {
+                    Console.WriteLine(name);
+                }
+            };
+            printToConsole(Console.ReadLine());
+        }
+    }
+}
