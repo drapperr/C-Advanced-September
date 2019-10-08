591f6098cebaa4771ba1f93794035a6975bb305a
drapperr <drapperr@users.noreply.github.com>
2019-10-08 15:42:41 +0300

    update

---
+++ Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,int> getSmallest = x=>x.Split(' ').Select(int.Parse).Min();
+            Console.WriteLine(getSmallest(Console.ReadLine())); ;
+        }
+    }
+}
