e253119ec449ed48fef5581d318243bfdd3d06b8
drapperr <drapperr@users.noreply.github.com>
2019-10-08 15:43:15 +0300

    update

---
+++ Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> add = x => x + 1;
+            Func<int, int> multiply = x => x * 2;
+            Func<int, int> subtract = x => x - 1;
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = numbers.Select(add).ToList();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(multiply).ToList();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(subtract).ToList();
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(" ", numbers));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
