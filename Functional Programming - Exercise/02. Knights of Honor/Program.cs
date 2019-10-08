226074c73f2d2efd61cc1d4348262a4b5d776342
drapperr <drapperr@users.noreply.github.com>
2019-10-08 15:42:26 +0300

    update

---
+++ Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printToConsole = x =>
+            {
+                foreach (var name in x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Console.WriteLine("Sir " + name);
+                }
+            };
+            printToConsole(Console.ReadLine());
+        }
+    }
+}
