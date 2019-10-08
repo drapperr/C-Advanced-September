fe0e787558e87d172e7258b73ed0c5531f0fbcac
drapperr <drapperr@users.noreply.github.com>
2019-10-08 15:42:58 +0300

    update

---
+++ Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> predicate = x => x % 2 == 0;
+
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int startNum = input[0];
+            int endNum = input[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "even")
+            {
+                numbers.RemoveAll(x=>!predicate(x));
+            }
+            else if (command == "odd")
+            {
+                numbers.RemoveAll(predicate);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
