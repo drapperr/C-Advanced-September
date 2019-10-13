6a03301429999fd5c4e32e77468d45539a480e66
drapperr <drapperr@users.noreply.github.com>
2019-10-13 19:32:45 +0300

    update

---
+++ Defining Classes - Exercise/1. Family/Family.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+            this.People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            var oldestMember = this.People[0];
+
+            foreach (var member in this.People)
+            {
+                if (member.Age > oldestMember.Age)
+                {
+                    oldestMember = member;
+                }
+            }
+            return oldestMember;
+        }
+
+        public List<Person> GetMoreThen30()
+        {
+            var members = new List<Person>();
+
+            foreach (var member in this.people)
+            {
+                if (member.Age>30)
+                {
+                    members.Add(member);
+                }
+            }
+            return members.OrderBy(x=>x.Name).ToList();
+        }
+    }
+}
---
+++ Defining Classes - Exercise/1. Family/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var family = new Family();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                var person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            var peopleMoreThan30 = family.GetMoreThen30();
+
+            foreach (var person in peopleMoreThan30)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
