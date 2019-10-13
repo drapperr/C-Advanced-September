00c1e9f4ea5aeb664a06eca8f4808f7617ba40b7
drapperr <drapperr@users.noreply.github.com>
2019-10-13 19:33:14 +0300

    update

---
+++ Defining Classes - Exercise/5. Date Modifier/DateModifier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        private string firstDate;
+        private string secondDate;
+
+        public DateModifier(string firstDate, string secondDate)
+        {
+            FirstDate = firstDate;
+            SecondDate = secondDate;
+
+            int[] firstDateInfo = firstDate
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            DateTime firstDateTime = new DateTime(firstDateInfo[0], firstDateInfo[1], firstDateInfo[2]);
+
+            int[] secondDateInfo = secondDate
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            DateTime secondDateTime = new DateTime(secondDateInfo[0], secondDateInfo[1], secondDateInfo[2]);
+
+            var diff = secondDateTime - firstDateTime;
+            Console.WriteLine(Math.Abs(diff.Days));
+        }
+
+        public string FirstDate
+        {
+            get { return firstDate; }
+            set { firstDate = value; }
+        }
+
+        public string SecondDate
+        {
+            get { return secondDate; }
+            set { secondDate = value; }
+        }
+
+    }
+}
