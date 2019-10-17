63bb987c5cc340650c05662208757275794c6da5
drapperr <drapperr@users.noreply.github.com>
2019-10-17 18:08:03 +0300

    update

---
+++ Defining Classes - Exercise/6. Speed Racing/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public double FuealAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TraveledDistance { get; set; }
+
+        public void Drive(double distance)
+        {
+            bool fuelIsEnough = distance * this.FuelConsumptionPerKilometer <= FuealAmount;
+            if (fuelIsEnough)
+            {
+                this.FuealAmount -= distance * this.FuelConsumptionPerKilometer;
+                this.TraveledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
