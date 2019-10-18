04f304d351b4e5fb6859e8d2dfb1dfaee58f2d96
drapperr <drapperr@users.noreply.github.com>
2019-10-18 19:37:13 +0300

    update

---
+++ Defining Classes - Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+        private int count;
+
+        public Parking(int capacity)
+        {
+            Capacity = capacity;
+            cars = new Dictionary<string, Car>();
+            count = 0;
+        }
+
+        public Dictionary<string, Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public int Count => count;
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (count == capacity)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car.RegistrationNumber, car);
+            count++;
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+            count--;
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars[registrationNumber];
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var number in registrationNumbers)
+            {
+                if (cars.ContainsKey(number))
+                {
+                    cars.Remove(number);
+                    count--;
+                }
+            }
+        }
+    }
+}
