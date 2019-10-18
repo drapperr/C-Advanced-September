4179ee86a082b23a78d5d5aebf10e07decf913f0
drapperr <drapperr@users.noreply.github.com>
2019-10-18 19:38:25 +0300

    update

---
+++ Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> noDevisible = (x, y) => x % y != 0;
+
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+            int divider = int.Parse(Console.ReadLine());
+
+            numbers = numbers.Where(x=>noDevisible(x,divider)).ToList();
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
