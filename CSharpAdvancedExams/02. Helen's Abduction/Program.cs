7d40b6da080d8342617b10c189082802d335a433
drapperr <drapperr@users.noreply.github.com>
2019-10-21 19:46:31 +0300

    update

---
+++ CSharpAdvancedExams/02. Helen's Abduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Helen_s_Abduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
