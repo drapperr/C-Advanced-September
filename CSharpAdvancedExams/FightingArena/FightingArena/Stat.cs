e24c674f3862b21fc8f27a4a900c410f92d58f01
drapperr <drapperr@users.noreply.github.com>
2019-10-22 15:44:43 +0300

    update

---
+++ CSharpAdvancedExams/FightingArena/FightingArena/Stat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Stat
+    {
+        public Stat(int strangth, int flexibility, int agility, int skills, int inteligence)
+        {
+            Strangth = strangth;
+            Flexibility = flexibility;
+            Agility = agility;
+            Skills = skills;
+            Inteligence = inteligence;
+        }
+
+        public int Strangth { get; set; }
+        public int Flexibility { get; set; }
+        public int Agility { get; set; }
+        public int Skills { get; set; }
+        public int Inteligence { get; set; }
+
+    }
+}
