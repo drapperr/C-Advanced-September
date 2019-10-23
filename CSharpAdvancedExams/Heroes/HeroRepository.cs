8a80c644cb7e5ec2cb8a3e8a911d88308589b91e
drapperr <drapperr@users.noreply.github.com>
2019-10-23 17:31:13 +0300

    update

---
+++ CSharpAdvancedExams/Heroes/HeroRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+        public HeroRepository()
+        {
+            data = new List<Hero>();
+        }
+        public int Count => data.Count;
+        public void Add(Hero hero)
+        {
+            data.Add(hero);
+        }
+        public void Remove(string name)
+        {
+            data.RemoveAll(x => x.Name == name);
+        }
+        public Hero GetHeroWithHighestStrength()
+        {
+           return data.OrderByDescending(x => x.Item.Strength).FirstOrDefault();
+        }
+        public Hero GetHeroWithHighestAbility()
+        {
+            return data.OrderByDescending(x => x.Item.Ability).FirstOrDefault();
+        }
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            return data.OrderByDescending(x => x.Item.Intelligence).FirstOrDefault();
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
