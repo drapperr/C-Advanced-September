fcec95259c749278f981458095218127245e21f9
drapperr <drapperr@users.noreply.github.com>
2019-10-23 17:31:44 +0300

    update

---
+++ CSharpAdvancedExams/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class Repository
+    {
+        private Dictionary<int, Person> data;
+        public int Count { get; private set; }
+        public Repository()
+        {
+            data = new Dictionary<int, Person>();
+        }
+        public void Add(Person person)
+        {
+            data.Add(Count, person);
+            Count++;
+        }
+        public Person Get(int id)
+        {
+            return data[id];
+        }
+        public bool Update(int id, Person newPerson)
+        {
+            if (!data.ContainsKey(id))
+            {
+                return false;
+            }
+
+            data[id] = newPerson;
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            if (!data.ContainsKey(id))
+            {
+                return false;
+            }
+
+            data.Remove(id);
+            Count--;
+            return true;
+        }
+    }
+}
