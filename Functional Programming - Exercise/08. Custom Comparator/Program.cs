9deb31d7f0ceddaa0ea231a07b8938c887f755ea
drapperr <drapperr@users.noreply.github.com>
2019-10-27 18:55:51 +0200

    update

---
+++ Functional Programming - Exercise/08. Custom Comparator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> evenNumbers = number.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            List<int> oddNumbers = number.Where(x => x % 2 != 0).OrderBy(x => x).ToList();
+
+            evenNumbers.AddRange(oddNumbers);
+
+            Console.WriteLine(string.Join(" ",evenNumbers));
+        }
+    }
+}
