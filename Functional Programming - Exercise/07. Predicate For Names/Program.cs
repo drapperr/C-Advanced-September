e0df1f4de5467c8f677c5e1c0632cd1802600990
drapperr <drapperr@users.noreply.github.com>
2019-10-27 18:55:32 +0200

    update

---
+++ Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,int, bool> func = (x,y) => x.Length <= y;
+
+            int maxLenght = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            List<string> filtredNames = names.Where(x => func(x, maxLenght)).ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine,filtredNames));
+        }
+    }
+}
