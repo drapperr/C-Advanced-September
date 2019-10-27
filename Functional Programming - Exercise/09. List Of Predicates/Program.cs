c6f10ee465952267484aeb7ce2181d0bfaab7aa8
drapperr <drapperr@users.noreply.github.com>
2019-10-27 18:56:12 +0200

    update

---
+++ Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, bool> func = (x, y) => x % y != 0;
+
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            List<int> resultNumbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                resultNumbers.Add(i);
+            }
+
+            foreach (var divider in dividers)
+            {
+                resultNumbers.RemoveAll(x => func(x, divider));
+            }
+
+            Console.WriteLine(string.Join(" ",resultNumbers));
+        }
+    }
+}
