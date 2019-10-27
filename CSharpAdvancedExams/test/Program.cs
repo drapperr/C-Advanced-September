74b98e521b7ff95011dfd3106fa8c7a7d130fb61
drapperr <drapperr@users.noreply.github.com>
2019-10-27 18:55:03 +0200

    update

---
+++ CSharpAdvancedExams/test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> input = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine(string.Join(" ",input));
+            Console.WriteLine();
+            Console.WriteLine(string.Join(" ",input));
+            Console.WriteLine();
+            Console.WriteLine(string.Join(" ",input));
+        }
+    }
+}
