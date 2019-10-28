1b5473dd66a8adab8570f2d980e1419947a90647
drapperr <drapperr@users.noreply.github.com>
2019-10-28 15:23:51 +0200

    update

---
+++ BasicAlgorithms/Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            long factorial = GetFactorial(number);
+            Console.WriteLine(factorial);
+        }
+
+        private static long GetFactorial(int number)
+        {
+            if (number==1)
+            {
+                return 1;
+            }
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
