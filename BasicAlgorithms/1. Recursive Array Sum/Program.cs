8e7b3c708ec377b593c94e12164f63106904372b
drapperr <drapperr@users.noreply.github.com>
2019-10-28 15:23:33 +0200

    update

---
+++ BasicAlgorithms/1. Recursive Array Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = SumArray(numbers);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int SumArray(int[] numbers,int i=0)
+        {
+            if (i==numbers.Length)
+            {
+                return 0;
+            }
+            int currentSum = numbers[i];
+            return currentSum + SumArray(numbers,++i);
+        }
+    }
+}
