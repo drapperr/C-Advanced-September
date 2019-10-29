2496677af113f1ff068fc094bdbde4767dc67e6c
drapperr <drapperr@users.noreply.github.com>
2019-10-29 10:38:21 +0200

    update

---
+++ Generics - Exercise/1. Generic Box of String/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericBox
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var box = new Box<double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input =double.Parse( Console.ReadLine());
+
+                box.Add(input);
+            }
+
+            var targetValue =double.Parse( Console.ReadLine());
+
+            Console.WriteLine(box.CountOfLargerElements(targetValue));
+        }
+    }
+}
