0c46082e45f04126a3524f19a66ea7a44fff8bb3
drapperr <drapperr@users.noreply.github.com>
2019-10-29 10:34:55 +0200

    update

---
+++ Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isEqualOrLangerFunc = 
+                (word, criteria) => word.Sum(x => x) >= criteria;
+
+            Func<string[],Func<string,int,bool>,int,string> myFunc =
+                (names, isLargerFunc, totalSum) => names.FirstOrDefault(x=>isLargerFunc(x,totalSum));
+
+            int targetSum = int.Parse(Console.ReadLine());
+
+            string[] inputNames = Console.ReadLine().Split();
+
+            string targetName = myFunc(inputNames, isEqualOrLangerFunc, targetSum);
+
+            Console.WriteLine(targetName);
+        }
+    }
+}
