27046d0fd937f12eba9ec4b3c5c385d603f6c721
drapperr <drapperr@users.noreply.github.com>
2019-10-30 10:30:34 +0200

    update

---
+++ Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public Lake()
+        {
+            elements = new List<T>();
+        }
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+        public void Add(List<T> elements)
+        {
+            this.elements.AddRange(elements);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return elements[i];
+                }
+            }
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return elements[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            Lake<int> lake = new Lake<int>();
+            lake.Add(numbers);
+
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
