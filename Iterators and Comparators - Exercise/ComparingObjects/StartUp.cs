73dfeb68ab9cea9dc6097201ae786f9df16d3fd0
drapperr <drapperr@users.noreply.github.com>
2019-10-30 10:29:54 +0200

    update

---
+++ Iterators and Comparators - Exercise/ComparingObjects/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] personInfo = input.Split();
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                string town = personInfo[2];
+
+                Person person = new Person(name, age, town);
+
+                people.Add(person);
+            }
+
+            int positon = int.Parse(Console.ReadLine());
+            Person targetPerson = people[positon - 1];
+            int matches = 0;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(targetPerson) == 0)
+                {
+                    matches++;
+                }
+            }
+
+            if (matches == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matches} {people.Count-matches} {people.Count}");
+            }
+        }
+    }
+}
