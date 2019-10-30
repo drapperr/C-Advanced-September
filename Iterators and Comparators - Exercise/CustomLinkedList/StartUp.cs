11930c45ace7e2bf80f06b6d04e862d3c5a17f2a
drapperr <drapperr@users.noreply.github.com>
2019-10-30 10:30:17 +0200

    update

---
+++ Iterators and Comparators - Exercise/CustomLinkedList/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Workshop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var myList = new DoubleLinkedList<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myList.AddFirst(i);
+            }
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name != other.Name)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return this.Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Person;
+
+            return item.Name == this.Name && item.Age == this.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this.Age.GetHashCode();
+        }
+    }
+}
