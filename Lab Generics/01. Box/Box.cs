3c9aa9b190496ed999b35c56f3689f322ef5ddbd
drapperr <drapperr@users.noreply.github.com>
2019-10-31 18:12:03 +0200

    update

---
+++ Lab Generics/01. Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public int Count => items.Count;
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+        public T Remove()
+        {
+            T element = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+            return element;
+        }
+    }
+}
