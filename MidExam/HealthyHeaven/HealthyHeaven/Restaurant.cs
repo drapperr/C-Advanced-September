85910a19c9ddecdc19c0e42c104ce9963c036702
drapperr <drapperr@users.noreply.github.com>
2019-11-01 22:22:40 +0200

    update

---
+++ MidExam/HealthyHeaven/HealthyHeaven/Restaurant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> data;
+
+        public Restaurant(string name)
+        {
+            Name = name;
+            data = new List<Salad>();
+        }
+
+        public string Name { get; set; }
+
+        public void Add(Salad salad)
+        {
+            data.Add(salad);
+        }
+
+        public bool Buy(string name)
+        {
+            if (data.RemoveAll(x=>x.Name==name)==0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Salad GetHealthiestSalad()
+        {
+            return data.OrderBy(x => x.GetTotalCalories()).FirstOrDefault();
+        }
+
+        public string GenerateMenu()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{Name} have {data.Count} salads:");
+
+            foreach (var salad in data)
+            {
+                sb.AppendLine(salad.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ MidExam/HealthyHeaven/HealthyHeaven/Salad.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Salad
+    {
+        private List<Vegetable> vegetables;
+
+        public Salad(string name)
+        {
+            Name = name;
+            vegetables = new List<Vegetable>();
+        }
+
+        public string Name { get; set; }
+
+        public int GetTotalCalories()
+        {
+            return vegetables.Sum(x => x.Calories);
+        }
+        public int GetProductCount()
+        {
+            return vegetables.Count();
+        }
+        public void Add(Vegetable product)
+        {
+            vegetables.Add(product);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"* Salad {Name} is {GetTotalCalories()} calories and have {GetProductCount()} products:");
+
+            foreach (var vegetable in vegetables)
+            {
+                sb.AppendLine(vegetable.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
