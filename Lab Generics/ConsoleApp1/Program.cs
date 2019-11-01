4a26e580db051db0ff662a8ce8641e99150629d6
drapperr <drapperr@users.noreply.github.com>
2019-11-01 22:22:20 +0200

    update

---
+++ Lab Generics/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("suadhiaudhiashdiudsh");
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
